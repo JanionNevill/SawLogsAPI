df56e726f6e5fcfccf5d501c658a63d2e37f9f3b
JanionNevill <janion.nevill@leicester.ac.uk>
2026-03-19 10:06:24 +0000

    Added reference to reservation

--- reservations/models.py
+++ reservations/models.py
@@ -6,9 +6,10 @@ from django.db.models import CASCADE
 class Reservation(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    reference = models.CharField(max_length=32, null=True, blank=True)
 
     def __str__(self):
         from logs.models import Log
 
         logs = Log.objects.filter(reservation=self)
-        return f"{self.id} - {self.user.username}: {len(logs)} logs"
+        return f"{self.reference if self.reference is not None else '#' + str(self.id)} - {self.user.username}: {len(logs)} logs"
